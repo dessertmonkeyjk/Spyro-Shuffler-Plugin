248915c4b4a093e51d48243bc85159272335bfd3
dessertmonkeyjk <dessert_monkeyjk@hotmail.com>
2022-07-03 11:50:27 -0500

    refined/optimize code

--- Release/spyro-shuffler.lua
+++ Release/spyro-shuffler.lua
@@ -7,7 +7,7 @@ plugin.settings = {}
 
 plugin.description =
 [[
-	*Version 07-02-2022*
+	*Version 07-03-2022*
 
 	Swaps games whenever a gem is collected in-game, as well as syncs gems across games.
 	Only gem total and hud are synced.
@@ -25,8 +25,14 @@ plugin.description =
 	-get_tag_from_hash_db returns tag, uses dat file to match key (hash) to name (value), returns tag (middle value)
 
 	To-do
-	-Rework code so swap trigger can be set by user (gems, dragon/orb/egg)
-	-Rework code so swap total can be set by user (gems, dragon/orb/egg)
+	-Rework code so swap trigger & swap total can be set by user (gems, dragon/orb/egg)
+		-Push code for triggering based on collectable into function(s)
+		-Put code for tracking collectables into function(s) 
+			1. (on swap) Get static x collectable count for current game from game table
+			2. (on swap) Add static total for all games x collectables from game table (for diff, HUD sync)
+			3. (on update) Get x collectable delta PER FRAME and for current swap
+			4. (on update) Check for PER FRAME swap threshold (maybe x collected for swap as well but moneybags...)
+			5. (pre swap) Add new x collectable gotten to current game in game table
 	-Fix issue for Spyro 1 HUD losing its mind when setting value to 0
 ]]
 
@@ -81,27 +87,32 @@ local gamedata = {
 function plugin.on_game_load(data, settings)
 	
 	--Get global data
-	local gt_coldstart = data.coldstart[gameinfo.getromhash()]
+	g_gamehash = gameinfo.getromhash()
+	gt_coldstart = data.coldstart[g_gamehash]
 	g_colthreshold = 5
-	
-	--If cold start is not set, assume game first boot is true
-	if gt_coldstart == nil then
-		data.coldstart[gameinfo.getromhash()] = true
-	end
-	
-	--Init first frame after cold start
-	g_coldframe = 0
-	
-	-- Get current game data, get real value, override after, set back before swap
+
+	-- Get current game data tag
 	g_tag = get_gametag()
 	if g_tag == 'none' then
 		console.log('!!Game not recognized!! Is it not in the database file?')
 	end
 	
-	-- 1 Get current game gem count from game table [gt_realcollectvar] (on swap)
+	--Set gamehash and game tag to game table
+	data.tags[g_gamehash] = g_tag
 
-	if data.collectvar[gameinfo.getromhash()] ~= nil then
-		gt_realcollectvar = data.collectvar[gameinfo.getromhash()]
+	--If cold start is not set, assume game first boot is true
+	if gt_coldstart == nil then
+		data.coldstart[g_gamehash] = true
+		gt_coldstart = data.coldstart[g_gamehash]
+	end
+	
+	--Init first frame after cold start
+	g_totalcurverset = false
+	g_coldframe = 0
+	
+	-- 1 Get current game gem count from game table [gt_realcollectvar] (on swap)
+	if data.collectvar[g_gamehash] ~= nil then
+		gt_realcollectvar = data.collectvar[g_gamehash]
 	else
 		gt_realcollectvar = 0
 	end
@@ -109,19 +120,11 @@ function plugin.on_game_load(data, settings)
 	-- 2 Add up current gem total from game table [g_totalcolvar] (on swap)
 	-- Add up total collect var so far
 	g_totalcolvar = 0
-	-- g_displaycollectdelta = 0
 	for key, value in pairs(data.collectvar) do 
 		g_totalcolvar = g_totalcolvar + value
 		local gametag = data.tags[key]
 		console.log(gametag, value)
 	end
-
-	-- Store initial collect value
-	gdf_lastcheckcollectvar = gd_curcollectvar
-	f_totalcurverset = false
-	
-	--Set global gamedata for current game
-	data.tags[gameinfo.getromhash()] = g_tag
 	
 	--Debug
 	local gamename = gameinfo.getromname()
@@ -133,43 +136,43 @@ end
 
 -- called each frame
 function plugin.on_frame(data, settings)
-	--Get global data
-	local gt_coldstart = data.coldstart[gameinfo.getromhash()]
 
 	--If cold start is true, assume false once gamedata value is at 0
 	if gt_coldstart == true then
 		if emu.framecount() >= 1600 then 
-			data.coldstart[gameinfo.getromhash()] = false
-			gt_coldstart = data.coldstart[gameinfo.getromhash()]
+			data.coldstart[g_gamehash] = false
+			gt_coldstart = data.coldstart[g_gamehash]
 			
 			console.log('Cold start is false')
 			return 	
 		end
 	end
-
-	--Use global var for game data from load fn (gd_curcollectvar, g_tag)
-	-- Get init collect, cur collect, and previous frame collect var
-	local gdf_curcollectvar = gamedata[g_tag].getgemvar()
 	
 	-- If cold start is false, then check if value increases afterwards
 	if gt_coldstart == false then
 
-		fcolvardelta = 0
-		fdeltacollectvar = 0
+		-- f_collastframedelta - Diff PER FRAME for x collectable pickup, used for threshold (always resets to 0)
+		-- f_colthisswap - Tracks how many x collectable gotten this swap, added to game table later pre-swap
+
+		-- Use global var for game data from load fn (gd_curcollectvar, g_tag)
+		-- Get init collect, cur collect, and previous frame collect var
+
+		local gdf_curcollectvar = gamedata[g_tag].getgemvar()
+		local f_collastframedelta = 0
+		f_colthisswap = 0
 		
 		-- Set cur var to game memory (not HUD)
 		-- Run before checking for gem change, update last checked var
 		
-		if f_totalcurverset == false then
-			local f_newgemval = g_totalcolvar + fdeltacollectvar
+		if g_totalcurverset == false then
 			gamedata[g_tag].setgemvar(g_totalcolvar)
 			
 			gdf_lastcheckcollectvar = g_totalcolvar
-			f_totalcurverset = true
+			g_totalcurverset = true
 		end
 		
 		-- 3 Get diff between current game gem count 
-		-- and total gem count from game table [fdeltacollectvar] (on update)
+		-- and total gem count from game table [f_colthisswap] (on update)
 		-- Check how many frames has passed since cold start/swap	
 		
 		if frames_since_restart >= 1 then
@@ -177,8 +180,8 @@ function plugin.on_frame(data, settings)
 		end
 		
 		if g_coldframe >= 2 then 
-			fcolvardelta = gdf_curcollectvar - gdf_lastcheckcollectvar
-			fdeltacollectvar = gdf_curcollectvar - g_totalcolvar	
+			f_collastframedelta = gdf_curcollectvar - gdf_lastcheckcollectvar
+			f_colthisswap = gdf_curcollectvar - g_totalcolvar	
 		end
 
 		-- Set HUD gem count PER FRAME
@@ -186,23 +189,21 @@ function plugin.on_frame(data, settings)
 			-- 	Why does it count one extra??
 		-- Need to check multiple Spyro 1 tags
 		if g_tag == "spyro1ntsc" then
-			local f_newgemval = g_totalcolvar + fdeltacollectvar - 1
+			local f_newgemval = g_totalcolvar + f_colthisswap - 1
 			if f_newgemval <= 0 then f_newgemval = 0 end
 			gamedata[g_tag].setgemhuds1var(f_newgemval)
 		else
-			local f_newgemval = g_totalcolvar + fdeltacollectvar
+			local f_newgemval = g_totalcolvar + f_colthisswap
 			gamedata[g_tag].setgemhudvar(f_newgemval)
 		end
 		
 		-- Debug
-		-- gui.drawText(10, 20, string.format("Var collect for swap: %d", fdeltacollectvar),0xFFFFFFFF, 0xFF000000, 16)
-	
-
+		gui.drawText(10, 20, string.format("Var collect for swap: %d", f_colthisswap),0xFFFFFFFF, 0xFF000000, 16)
 		
 	-- Run collect change check, delay so total collect change is set first
 		if g_coldframe >= 2 then
 			-- If pre is higher than cur, swap, otherwise set pre to cur on next frame
-			if fcolvardelta == g_colthreshold then
+			if f_collastframedelta >= g_colthreshold then
 				swap_game()
 			else
 				gdf_lastcheckcollectvar = gdf_curcollectvar
@@ -211,7 +212,7 @@ function plugin.on_frame(data, settings)
 end
 
 	-- Debug
-	-- gui.drawText(10, 5, string.format("Var collected: %d", g_totalcolvar), 0xFFFFFFFF, 0xFF000000, 16)
+	gui.drawText(10, 5, string.format("Var collected: %d", g_totalcolvar), 0xFFFFFFFF, 0xFF000000, 16)
 	gui.drawText(10, 35, string.format("Gem threshold: %d", g_colthreshold),0xFFFFFFFF, 0xFF000000, 16)
 	gui.drawText(10, 50, string.format("Game tag: %s", g_tag),0xFFFFFFFF, 0xFF000000, 16)
 	
@@ -219,20 +220,19 @@ end
 
 -- called each time a game/state is saved (before swap)
 function plugin.on_game_save(data, settings)
-
-	local oldgemval = data.collectvar[gameinfo.getromhash()]
 	
 	-- Add last gem total from game data table and gems collected this swap, add back into table
 	-- 4 Add diff gems to REAL gem count (pre-swap)
-	local newgemval = gt_realcollectvar + fdeltacollectvar
-	data.collectvar[gameinfo.getromhash()] = newgemval
+	local newgemval = gt_realcollectvar + f_colthisswap
+	data.collectvar[g_gamehash] = newgemval
 	
 	-- Wait until swap total is updated before next swap var check
-	f_totalcurverset = false
+	g_totalcurverset = false
 	
 	-- Debug
-	console.log('before set', f_totalcurverset)
-	-- console.log(oldgemval,fdeltacollectvar,newgemval)
+	local oldgemval = data.collectvar[g_gamehash]
+	console.log('before set', g_totalcurverset)
+	-- console.log(oldgemval,f_colthisswap,newgemval)
 	-- console.log('---')
 end
 
